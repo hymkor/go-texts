782382a6250f2785537f9cf47969de3f91b9c222
Kaoru HAYAMA <iyahaya@nifty.com>
2018-07-12 23:33:13 +0900

    Filter class supports the last-line without \n

--- filter.go
+++ filter.go
@@ -8,44 +8,29 @@ import (
 // Filter is the converter for io.Reader
 type Filter struct {
 	r          io.Reader
-	sc         *bufio.Scanner
+	br         *bufio.Reader
 	buffer     []byte
 	translator func([]byte) ([]byte, error)
 }
 
 // Read is the method satisfy the requirements of io.Reader
 func (this *Filter) Read(p []byte) (int, error) {
-	if this.r == nil {
-		if err := this.sc.Err(); err != nil {
-			return 0, err
-		}
-		return 0, io.EOF
-	}
 	for len(this.buffer) < len(p) {
-		if !this.sc.Scan() {
-			if len(this.buffer) <= 0 {
-				this.r = nil
-				if err := this.sc.Err(); err != nil {
-					return 0, err
-				}
-				return 0, io.EOF
-			}
-			for i := 0; i < len(this.buffer); i++ {
-				p[i] = this.buffer[i]
-			}
-			this.r = nil
-			return len(this.buffer), nil
-		}
-		line, err := this.translator(this.sc.Bytes())
-		if err != nil {
-			return 0, err
-		}
+		line, err1 := this.br.ReadBytes('\n')
+		line, err2 := this.translator(line)
 		this.buffer = append(this.buffer, line...)
-		this.buffer = append(this.buffer, '\n')
+		if err1 != nil || err2 != nil {
+			result := copy(p, this.buffer)
+			var err error
+			if err1 != nil {
+				err = err1
+			} else {
+				err = err2
+			}
+			return result, err
+		}
 	}
-	for i := 0; i < len(p); i++ {
-		p[i] = this.buffer[i]
-	}
+	copy(p, this.buffer)
 	this.buffer = this.buffer[len(p):]
 	return len(p), nil
 }
@@ -54,7 +39,7 @@ func (this *Filter) Read(p []byte) (int, error) {
 func NewFilter(fd io.Reader, translator func([]byte) ([]byte, error)) io.Reader {
 	return &Filter{
 		r:          fd,
-		sc:         bufio.NewScanner(fd),
+		br:         bufio.NewReader(fd),
 		translator: translator,
 	}
 }
