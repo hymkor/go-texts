717f826f352d4c7598b2bb208cd3ecc1ce119e30
Kaoru HAYAMA <iyahaya@nifty.com>
2018-10-18 21:13:49 +0900

    Add reader_unix.go

---
+++ mbcs/reader_unix.go
@@ -0,0 +1,13 @@
+// +build !windows
+
+package mbcs
+
+import "io"
+
+func NewAutoDetectReader(fd io.Reader, _ uintptr) io.Reader {
+	return fd
+}
+
+func NewAtoUReader(fd io.Reader, _ uintptr) io.Reader {
+	return fd
+}
