a285bf3bb9d8008e3520d8daa0fd15ec2f5367d1
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-02-03 15:42:45 +0900

    Add test for the case the last line has no LF

---
+++ mbcs/tst_lastLineHasNoLF.go
@@ -0,0 +1,31 @@
+// +build ignore
+
+// Test case: source stream's last line has no LF.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+
+	"github.com/zetamatta/go-texts/mbcs"
+)
+
+func main() {
+	reader := mbcs.NewAtoUReader(strings.NewReader("1stLine\nNoLfLine"),
+		mbcs.ConsoleCP())
+	sc := bufio.NewScanner(reader)
+	fmt.Println("--- mbcs.NewAtoUReader ---")
+	for sc.Scan() {
+		fmt.Println(sc.Text())
+	}
+
+	fmt.Println("--- mbcs.NewAutoDetectReader ---")
+	reader = mbcs.NewAutoDetectReader(strings.NewReader("1stLine\nNoLfLine"),
+		mbcs.ConsoleCP())
+	sc = bufio.NewScanner(reader)
+	for sc.Scan() {
+		fmt.Println(sc.Text())
+	}
+}
